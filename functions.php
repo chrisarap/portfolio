f5e2f890bfd9927dce98ecc56af4ebdaa3ab9f8a
chrisarap <carteaga460@gmail.com>
2021-05-14 22:19:11 -0400

    adding media to navbar

---
+++ functions.php
@@ -0,0 +1,24 @@
+<?php 
+	function media(){
+		echo 
+			'<a href="">
+				<img class="contact-img" src="http://assets.stickpng.com/images/584ac2d03ac3a570f94a666d.png">
+			</a>
+			
+			<a href="">
+				<img class="contact-img" src="https://image.flaticon.com/icons/png/512/174/174857.png">
+			</a>
+
+			<a href="https://wa.link/aiw58w" target="_blank">
+				<img class="contact-img" src="https://upload.wikimedia.org/wikipedia/commons/thumb/1/19/WhatsApp_logo-color-vertical.svg/294px-WhatsApp_logo-color-vertical.svg.png">
+			</a>
+
+			<a href="mailto:carteaga460@gmail.com?">
+				<img class="contact-img" src="https://i1.wp.com/9to5google.com/wp-content/uploads/sites/4/2020/10/workspace-new-gmail-icon.png?ssl=1">	
+			</a>
+
+			<a href="https://msng.link/o/?chrisarap=tg">
+				<img class="contact-img" src="http://assets.stickpng.com/images/5842a8fba6515b1e0ad75b03.png">	
+			</a>';
+	}
+?>
--- index.php
+++ index.php
@@ -6,32 +6,39 @@
 	<link href="https://fonts.googleapis.com/css?family=Ubuntu" rel="stylesheet">
 	<meta name="viewport" content="width=device-width, initial-scale=1.0"&amp;gt>
 	<meta charset="utf-8">
-	<link href='https://css.gg/facebook.css' rel='stylesheet'>
+	<?php
+		include "functions.php";
+	?> 
 </head>
 <body>
 
 <nav>
 	<ul>
-		<a href="#section-about">
+		<a class="nav-link" href="#section-about">
 			<li>About</li>
 		</a>
 
-		<a href="#section-work">
+		<a class="nav-link" href="#section-work">
 			<li>Work</li>
 		</a>
 
-		<a href="#section-certificate">
+		<a class="nav-link" href="#section-certificate">
 			<li>Certificates</li>
 		</a>
 
-		<a href="#section-tech">
+		<a class="nav-link" href="#section-tech">
 			<li>Technologies</li>
 		</a>
 
-		<a href="#section-contact">
+		<a class="nav-link" href="#section-contact">
 			<li>Contact</li>
 		</a>
-	</ul>	
+	</ul>
+	<div class="nav-media">
+		<?php 
+			media();
+		?>	
+	</div>
 </nav>
 
 <section id="section-about" class="section">
@@ -104,27 +111,12 @@
 
 <section id="section-contact" class="section">
 
-	
-	<a href="">
-		<img class="contact-img" src="http://assets.stickpng.com/images/584ac2d03ac3a570f94a666d.png">
-	</a>
-
-	
-	<a href="">
-		<img class="contact-img" src="https://image.flaticon.com/icons/png/512/174/174857.png">
-	</a>
-	<a href="https://wa.link/aiw58w" target="_blank">
-		<img class="contact-img" src="https://upload.wikimedia.org/wikipedia/commons/thumb/1/19/WhatsApp_logo-color-vertical.svg/294px-WhatsApp_logo-color-vertical.svg.png">
-	</a>
-	<a href="mailto:carteaga460@gmail.com?">
-		<img class="contact-img" src="https://i1.wp.com/9to5google.com/wp-content/uploads/sites/4/2020/10/workspace-new-gmail-icon.png?ssl=1">	
-	</a>
-	<a href="https://msng.link/o/?chrisarap=tg">
-		<img class="contact-img" src="http://assets.stickpng.com/images/5842a8fba6515b1e0ad75b03.png">	
-	</a>
+	<?php 
+		media();
+	?>
 	
 
-	</ul>
+	
 </section>
 
 </body>
