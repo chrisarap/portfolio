a87612c5c07b7f207a2fbb017d932dd23b6e5e83
chrisarap <carteaga460@gmail.com>
2021-05-18 21:59:21 -0400

    icon change

--- index.php
+++ index.php
@@ -72,6 +72,12 @@
 			<h2>UDO localhost</h2>
 			<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse vitae tortor diam  in ullamcorper malesuada.</p>
 			<div class="work-links-box">
+				<a href=""  				class="work-link" target="_blank">html</a>
+				<a href=""  				class="work-link" target="_blank">css</a>
+				<a href=""  				class="work-link" target="_blank">php</a>
+				<a href=""  				class="work-link" target="_blank">sql</a>
+			</div>
+			<div class="work-links-box">
 				<a href="../udo/index.php"  				class="work-link" target="_blank">Demo</a>
 				<a href="https://github.com/chrisarap/udo" 	class="work-link" target="_blank">Repo</a>
 			</div>
@@ -94,46 +100,43 @@
 		
 </section>
 
-<section>
-	<h2>About</h2>
-	<p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.
-</p>
-<button>Download cv</button>
+	<section>
+		<h2>About</h2>
+		<p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.</p>
+	<button>Download cv</button>
 </section>
 
 <section id="section-tech" class="section">
 	<h2>Technologies</h2>
 	<div class="tech-box">
-		<img class="tech_logo" src="https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Flogos-download.com%2Fwp-content%2Fuploads%2F2017%2F07%2FHTML5_badge.png&f=1&nofb=1">
+		<img class="tech_logo" src="img/icons/html.png">
 		<p>html</p>
 	</div>
 
 	<div class="tech-box">
-		<img class="tech_logo" src="https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fmaxcdn.icons8.com%2FShare%2Ficon%2FLogos%2Fcss31600.png&f=1&nofb=1">
+		<img class="tech_logo" src="img/icons/css.png">
 		<p>css</p>
 	</div>
 
 	<div class="tech-box">
-		<img class="tech_logo" src="https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fupload.wikimedia.org%2Fwikipedia%2Fcommons%2Fthumb%2F9%2F99%2FUnofficial_JavaScript_logo_2.svg%2F1200px-Unofficial_JavaScript_logo_2.svg.png&f=1&nofb=1">
+		<img class="tech_logo" src="img/icons/js.png">
 		<p>javascript</p>
 	</div>
 
 	<div class="tech-box">
-		<img class="tech_logo" src="https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fcdn.freebiesupply.com%2Flogos%2Flarge%2F2x%2Fphp-logo-png-transparent.png&f=1&nofb=1">
+		<img class="tech_logo" src="img/icons/php.png">
 		<p>php</p>
 	</div>
 
 	<div class="tech-box">
-		<img class="tech_logo" src="http://assets.stickpng.com/images/58481057cef1014c0b5e4951.png">
+		<img class="tech_logo" src="img/icons/sql.png">
 		<p>my sql</p>
 	</div>
 
 	<div class="tech-box">
-		<img class="tech_logo" src="http://assets.stickpng.com/images/5847f981cef1014c0b5e48be.png">
+		<img class="tech_logo" src="img/icons/git.png">
 		<p>git</p>
 	</div>
-
-
 </section>
 
 <section id="section-certificate" >
@@ -159,8 +162,11 @@
 </section>
 
 <div class="up-btn">
-	<a href="#section-about">
-		<img src="https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fupload.wikimedia.org%2Fwikipedia%2Fcommons%2Fthumb%2F7%2F76%2FWhite_Arrow_Up.svg%2F480px-White_Arrow_Up.svg.png&f=1&nofb=1">
+	<a  href="#section-about">
+		<div class="up-btn-box">
+			<img src="img/icons/up.png">
+			<img src="img/icons/up.png">
+		</div>
 	</a>
 </div>
 
@@ -169,27 +175,28 @@
 		
 		<div class="media-logo">
 			<a href="">
-				<img class="contact-img" src="http://assets.stickpng.com/images/584ac2d03ac3a570f94a666d.png">
+				<img class="contact-img" src="img/icons/facebook.png">
 			</a>
 			
 			<a href="">
-				<img class="contact-img" src="https://image.flaticon.com/icons/png/512/174/174857.png">
+				<img class="contact-img" src="img/icons/linkedin.png">
 			</a>
 
 			<a href="https://wa.link/aiw58w" target="_blank">
-				<img class="contact-img" src="https://upload.wikimedia.org/wikipedia/commons/thumb/1/19/WhatsApp_logo-color-vertical.svg/294px-WhatsApp_logo-color-vertical.svg.png">
-			</a>
-
-			<a href="mailto:carteaga460@gmail.com?">
-				<img class="contact-img" src="https://i1.wp.com/9to5google.com/wp-content/uploads/sites/4/2020/10/workspace-new-gmail-icon.png?ssl=1">	
+				<img class="contact-img" src="img/icons/whatsapp.png">
 			</a>
 
 			<a href="https://msng.link/o/?chrisarap=tg">
-				<img class="contact-img" src="http://assets.stickpng.com/images/5842a8fba6515b1e0ad75b03.png">	
+				<img class="contact-img" src="img/icons/telegram.png">	
 			</a>
 
+			<a href="mailto:carteaga460@gmail.com?">
+				<img class="contact-img" src="img/icons/gmail.png">	
+			</a>
+
+			
 			<a href="https://github.com/chrisarap">
-				<img class="contact-img" src="https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Flogos-download.com%2Fwp-content%2Fuploads%2F2016%2F09%2FGitHub_logo.png&f=1&nofb=1">
+				<img class="contact-img" src="img/icons/github.png">
 			</a>
 		</div>
 </section>
