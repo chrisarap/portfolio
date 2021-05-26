a6d84daedc81a35913e6736e39e17c1178a937a5
chrisarap <carteaga460@gmail.com>
2021-05-25 21:08:52 -0400

    about-section finished

--- index.php
+++ index.php
@@ -87,8 +87,8 @@
 
 <section id="section-about">
 	<h2>About</h2>
-	<p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.</p>
-	<button>Download cv</button>
+	<p>I'm Christian Arteaga, a Venezuelan web developer and engineer. I like to solve problems with code and never stop learning. First it works and then the decoration, always simple and straightforward. Occasional chess player, passionate about technology, why and how things work. If you want to talk, work or play <a href="https://lichess.org/@/chrisar75" target="_blank">chess</a> with me, just <a href="#section-contact">let me know</a>.</p>
+	<!--<button>Download cv</button>-->
 </section>
 
 <section id="section-tech" class="section">
